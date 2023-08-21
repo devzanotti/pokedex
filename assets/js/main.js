4f9cbdebc144b14fde15dd7ff20a58f5403c3a27
Gustavo Zanotti <guzanottis@gmail.com>
2023-08-21 17:37:00 -0300

    html dinamico, incluindo nome, tipo, id

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 
 function convertPokemonToLi(pokemon){
     return `
-        <li class="pokemon">
-                    <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.id}</span>
                     <span class="name">${pokemon.name}</span>
                     
                     <div class="detail">
                         <ol class="types">
-                            <li class="type">grass</li>
-                            <li class="type">poison</li>
+                            ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
                         </ol>
 
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                        <img src="${pokemon.photo}"
+                             alt="${pokemon.name}">
                     </div>                
                 </li>           
      `
@@ -20,8 +20,7 @@ function convertPokemonToLi(pokemon){
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => {
-        pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')        
-    })
+    pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join('')        
+})
 
-    .catch((error)  => console.error(error))
 
